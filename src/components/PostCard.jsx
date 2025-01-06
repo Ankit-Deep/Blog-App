9f80a36934007e14846b4e9ec8689f908d4e2452
Ankit Deep <AnkitDeep01@outlook.com>
2025-01-06 13:24:33 +0530

    Updating favicon

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -110,7 +110,7 @@ function PostCard({
   return (
     <>
       <Link to={`/post/${$id}`} className="">
-        <div className="border-2 border-black w-full h-full bg-[#8191a7] rounded-lg shadow-gray-700 shadow-2xl hover:shadow-2xl hover:shadow-gray-950 flex flex-col p-4 hover:p-[20px] duration-200 ">
+        <div className="border border-black w-full h-full bg-[#8191a7] rounded-lg shadow-gray-700 shadow-2xl hover:shadow-2xl hover:shadow-gray-950 flex flex-col p-4 hover:p-[20px] duration-200 ">
 
           {/* User profile starts */}
           <div className=" flex items-center gap-2">
@@ -122,7 +122,7 @@ function PostCard({
               {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                 <path d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z" />
               </svg> */}
-              
+
             </span>
             <h3
               className="font-medium text-sm hover:underline"
